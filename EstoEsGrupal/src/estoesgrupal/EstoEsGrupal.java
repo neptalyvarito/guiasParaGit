4800b0ca873db82a804df6091f6471e4c320f209
Edwin Amilcar chura yucra <edwinamilcarchurayucra7@gmail.com>
2025-04-26 21:38:15 -0500

    se agregó la funcion pedirDatos para que el usuario ingrese su nombre y apellido

--- EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
+++ EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
@@ -1,15 +1,24 @@
 
 package estoesgrupal;
 
+import java.util.Scanner;
+
 public class EstoEsGrupal {
 
     public static void main(String[] args) {
 
-        String nombre = "Alvaro ";
-        String apellido = "Mayo";
-        String nombreCompleto = nombre + apellido;
-        System.out.println("Hola Mundo");
+        String nombre = pedirDatos("ingresa tu nombre: ");
+        String apellido = pedirDatos("ingresa tu apellido: ");
+        String nombreCompleto = nombre + " " + apellido;
+        System.out.println("Hola Mundo !!!!");
         System.out.println("De parte de: " + nombreCompleto);
+    }
+
+    private static String pedirDatos(String datoPedir) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(datoPedir);
+        String texto = sc.nextLine();
+        return texto;
     }
 
 }
