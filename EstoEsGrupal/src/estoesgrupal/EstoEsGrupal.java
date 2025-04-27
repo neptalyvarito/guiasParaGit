c473ecb69cc1c8c86e0ff8ef3055af542ae25b26
Edwin Amilcar chura yucra <edwinamilcarchurayucra7@gmail.com>
2025-04-26 20:56:59 -0500

    comit arreglando la consonante de hola mundo

--- EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
+++ EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
@@ -1,17 +1,15 @@
 
 package estoesgrupal;
 
-
 public class EstoEsGrupal {
 
-   
     public static void main(String[] args) {
-      
-        String nombre ="Alvaro ";
+
+        String nombre = "Alvaro ";
         String apellido = "Mayo";
         String nombreCompleto = nombre + apellido;
-        System.out.println("Hola Munda");
+        System.out.println("Hola Mundo");
         System.out.println("De parte de: " + nombreCompleto);
     }
-    
+
 }
