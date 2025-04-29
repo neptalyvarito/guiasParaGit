67eabf7b3e1c74029392d2a3ba5adb7fec2167dd
Elias Diaz <eliasalejandrodc@hotmail.com>
2025-04-28 19:50:28 -0500

    feat: saludar por pais escogido

--- EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
+++ EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
@@ -1,4 +1,3 @@
-
 package estoesgrupal;
 
 import java.util.Scanner;
@@ -7,11 +6,42 @@ public class EstoEsGrupal {
 
     public static void main(String[] args) {
 
-        String nombre = pedirDatos("ingresa tu nombre: ");
-        String apellido = pedirDatos("ingresa tu apellido: ");
-        String nombreCompleto = nombre + " " + apellido;
-        System.out.println("Hola Mundo !!!!  espero que estes bien");
-        System.out.println("De parte de: " + nombreCompleto);
+        String nombreCompleto = pedirDatos("Ingresa tu nombre: ") + " " + pedirDatos("Ingresa tu apellido: ");
+        Integer opcion;
+        System.out.print("\n");
+
+        do {
+
+            System.out.println("Quieres saludar al mundo o a un pais?: ");
+            System.out.println("1: Al mundo");
+            System.out.println("2: A un pais");
+            System.out.println("3: A un pais");
+            System.out.print("> ");
+            Scanner sc = new Scanner(System.in);
+            opcion = Integer.valueOf(sc.nextLine());
+            System.out.print("\n");
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Hola Mundo!!!!  Espero que estes bien.");
+                    System.out.println("De parte de: " + nombreCompleto);
+                    break;
+                case 2:
+                    System.out.print("A que pais quiere saludar?: ");
+                    String pais = sc.nextLine();
+                    System.out.println("\nHola " + pais + "!!! Espero que estes bien.");
+                    System.out.println("De parte de: " + nombreCompleto);
+                    break;
+                case 3:
+                    System.out.println("Adios. Lindo dia!!!");
+                    break;
+                default:
+                    System.out.println("Ingrese una de las opciones mostradas!");
+                    break;
+            }
+            System.out.println("");
+            
+        } while (opcion != 3);
     }
 
     private static String pedirDatos(String datoPedir) {
