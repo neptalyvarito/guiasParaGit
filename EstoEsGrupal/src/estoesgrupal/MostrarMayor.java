55dd800d5b5ddd7734618800faf0c527b0be6121
Brei <catashungabreider@gmail.com>
2025-04-29 02:44:10 -0500

    Nos muestra el número mayor

--- EstoEsGrupal/src/estoesgrupal/MostrarMayor.java
+++ EstoEsGrupal/src/estoesgrupal/MostrarMayor.java
@@ -5,6 +5,26 @@ public class MostrarMayor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.println("--------------");
+        System.out.print("Ingrese n1: ");
+        int n1 = sc.nextInt();
+        System.out.print("Ingrese n2: ");
+        int n2 = sc.nextInt();
+        System.out.print("Ingrese n3: ");
+        int n3 = sc.nextInt();
+        System.out.println("--------------");
+
+        int mayor = n1;
+        if(n2>mayor){
+            mayor = n2;
+        }
+        if(n3 > mayor){
+            mayor = n3;
+        }
+
+        System.out.println("--------------");
+        System.out.println("El mayor es: " + mayor);
+        System.out.println("--------------");
+
     }
 }
