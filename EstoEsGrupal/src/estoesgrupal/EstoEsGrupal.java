abc2f5249b71ed056efc07bb5d62098476119575
Brei <catashungabreider@gmail.com>
2025-04-29 03:00:38 -0500

    arreglar error

--- EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
+++ EstoEsGrupal/src/estoesgrupal/EstoEsGrupal.java
@@ -4,38 +4,50 @@ import java.util.Scanner;
 
 public class EstoEsGrupal {
 
+    private static final Scanner sc = new Scanner(System.in); // Instancia única de Scanner
+
     public static void main(String[] args) {
 
-        textoSubrayado(" PROGRAMA PARA SALUDOS");
+        textoSubrayado("PROGRAMA PARA SALUDOS");
         String nombreCompleto = pedirDatos("Ingresa tu nombre: ") + " " + pedirDatos("Ingresa tu apellido: ");
-        Integer opcion;       
+        int opcion;
 
         do {
+            textoSubrayado(
+                "¿Quieres saludar al mundo o a un país?:\n" +
+                "1: Al mundo\n" +
+                "2: A un país\n" +
+                "3: Salir"
+            );
 
-            textoSubrayado("""
-                           Quieres saludar al mundo o a un pais?:
-                           1: Al mundo
-                           2: A un pais
-                           3: Salir""");
-            opcion = Integer.valueOf(pedirDatos("> "));
+            try {
+                opcion = Integer.parseInt(pedirDatos("> "));
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, ingresa un número válido.");
+                opcion = -1;
+                continue;
+            }
+
             System.out.println("-------------------------");
 
             switch (opcion) {
                 case 1:
-                    Saludo("Mundo", nombreCompleto);
+                    saludo("Mundo", nombreCompleto);
                     break;
                 case 2:
-                    Saludo(pedirDatos("A que pais quiere saludar?: "), nombreCompleto);
+                    saludo(pedirDatos("¿A qué país quieres saludar?: "), nombreCompleto);
                     break;
                 case 3:
-                    System.out.println("Adios. Lindo dia!!!");
+                    System.out.println("Adiós. ¡Lindo día!");
                     break;
                 default:
-                    System.out.println("Ingrese una de las opciones mostradas!");
+                    System.out.println("¡Ingresa una de las opciones mostradas!");
                     break;
             }
 
         } while (opcion != 3);
+
+        sc.close(); // Cierra el Scanner al final
     }
 
     private static void textoSubrayado(String texto) {
@@ -44,16 +56,13 @@ public class EstoEsGrupal {
         System.out.println("-----------------------");
     }
 
-    private static void Saludo(String pais, String nombreCompleto) {
-        System.out.println("Hola " + pais + "!!! Espero que estes bien.");
+    private static void saludo(String pais, String nombreCompleto) {
+        System.out.println("¡Hola " + pais + "! Espero que estés bien.");
         System.out.println("De parte de: " + nombreCompleto);
     }
 
-    private static String pedirDatos(String datoPedir) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print(datoPedir);
-        String texto = sc.nextLine();
-        return texto;
+    private static String pedirDatos(String mensaje) {
+        System.out.print(mensaje);
+        return sc.nextLine();
     }
-
 }
